f4b6548592d314416b04c233ea90d31fdd93a6f5
William A. Kennington III <william@wkennignton.com>
2013-05-03 16:05:18 -0500

    Basic Argument Parser

--- main.py
+++ main.py
@@ -19,14 +19,35 @@
 """
 import smtp
 import configparser
+import argparse
+
+def serve(config):
+    addr = config['Global'].get('addr', '0.0.0.0')
+    port = config['Global'].get('port', 25)
+    host = config['Global'].get('host', 'localhost')
+    server = smtp.SMTP(host=host, port=port, addr=addr)
+    server.run()
+    exit(0)
 
 def run():
+    # Parse the command line arguments
+    parser = argparse.ArgumentParser(description='Spawn the spampot server')
+    parser.add_argument('--conf', dest='conf', metavar='c', type=str, default='spampot.conf', help='Configuration file to read')
+
+    args = parser.parse_args()
+
+    # Read the default configuration file
     config = configparser.ConfigParser()
-    config.read('spampot.conf')
+    config.read(args.conf)
     if not ('Global' in config.sections()):
         print('Configuration file is missing the "Global" section')
         exit(1)
-    server = smtp.SMTP()
+    log = config['Global'].get('log', 'syslog')
+    if config['Global'].get('daemon', True):
+        if log == '-':
+            pass
+    else:
+        serve(config)
     exit(0)
 
 if  __name__ == '__main__':
